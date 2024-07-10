810fa195f690459c1188da0f8e4685d06268c466
ALPER TUNGA <alper@altu.dev>
2024-07-10 22:23:40 +0300

    [Session]; 10/07/2024 - Password Generator 2

---
+++ YA/YA.BlazorWasmStarter/Enums/CheckBoxActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace YA.BlazorWasmStarter.Enums
+{
+    public enum CheckBoxActionType
+    {
+        Numbers=1,
+        Lowercase=2,
+        Uppercase=3,
+        SpecialChars=4
+    }
+}
