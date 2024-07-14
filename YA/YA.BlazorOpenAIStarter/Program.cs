d84e8dcce9bd06018a667898793a5898585a1e21
ALPER TUNGA <alper@altu.dev>
2024-07-14 13:07:55 +0300

    [Session]: 14/07/2024 - AI Part.

--- YA/YA.BlazorOpenAIStarter/Program.cs
+++ YA/YA.BlazorOpenAIStarter/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Extensions;
 using YA.BlazorOpenAIStarter;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var openAIApiKey = builder
+    .Configuration
+    .GetSection("OpenAIApiKey")
+    .Value;
+
+builder.Services.AddOpenAIService(settings => settings.ApiKey = openAIApiKey);
 
 await builder.Build().RunAsync();
