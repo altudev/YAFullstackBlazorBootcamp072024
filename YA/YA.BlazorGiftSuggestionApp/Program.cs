1324934fd7665bea3cf7850959eb81910b7a7564
ALPER TUNGA <alper@altu.dev>
2024-07-21 13:09:44 +0300

    [Session]: 21/07/2024 GiftSuggestionApp has been completed.

--- YA/YA.BlazorGiftSuggestionApp/Program.cs
+++ YA/YA.BlazorGiftSuggestionApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OpenAI.Extensions;
@@ -15,5 +16,7 @@ var openAIApiKey = builder
     .Value;
 
 builder.Services.AddOpenAIService(settings => settings.ApiKey = openAIApiKey);
+
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
